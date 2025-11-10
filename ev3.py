4cf831b1015b30277a1e00da16d01b0c29d25e8c
BuildTools <unconfigured@null.spigotmc.org>
2025-11-10 12:22:43 +0100

    connection demo

--- ev3.py
+++ ev3.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 from ev3dev2.display import Display
+from ev3dev2.sound import Sound
 import ev3dev2.fonts as fonts
 import socket
 import time
@@ -8,10 +9,14 @@ HOST = "192.168.1.31"
 PORT = 5001
 DEVICE_NAME = "EV3-1"
 
+# display
 screen = Display()
 screen.clear()
 screen.update()
 _font = fonts.load('helvB24')
+
+# sound
+spkr = Sound()
 
 while True:
     try:
@@ -28,6 +33,7 @@ while True:
         print("Sent device name:", DEVICE_NAME)
         screen.clear()
         screen.draw.text((10, 10), 'Connected', font=_font)
+        spkr.speak(text="EV3 connected", volume=100)
         screen.update()
 
     except Exception as e:
